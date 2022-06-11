36a5a4db89d5240c786c66eee64fab7374caf9cb
Jon Heise <j.heise@gmail.com>
2022-06-11 16:03:53 -0700

    adding pipfile and updating to python3

--- alarm.py
+++ alarm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# git@github.com:jheise/alarmclock.git!/usr/bin/env python
 
 import os
 import sys
@@ -7,6 +7,7 @@ import signal
 import argparse
 
 from syslog import syslog
+
 
 def play_alarm(alarm):
     syslog("starting loop {0}".format(times))
@@ -17,8 +18,12 @@ def play_alarm(alarm):
 
 
 parser = argparse.ArgumentParser(description="Run an Alarm")
-parser.add_argument("-d", "--dry-run", action="store_true", help="play sound 3 times and stop")
-parser.add_argument("alarm", default="bell.mp3", help="What sound to play, default is bell.mp3")
+parser.add_argument(
+    "-d", "--dry-run", action="store_true", help="play sound 3 times and stop"
+)
+parser.add_argument(
+    "alarm", default="bell.mp3", help="What sound to play, default is bell.mp3"
+)
 args = parser.parse_args()
 
 dry_run = args.dry_run
@@ -32,19 +37,21 @@ default = "bell.mp3"
 running = True
 
 current = "/home/pi/alarm/alarms/" + pid
-with open(current,"w") as fh:
-	fh.write(pid+"\n")
+with open(current, "w") as fh:
+    fh.write(pid + "\n")
 
 alarm = sounds + args.alarm
 if not os.path.isfile(alarm):
-	alarm = sounds+default
+    alarm = sounds + default
 
 syslog("playing %s" % alarm)
 
+
 def stopHandler(signum, frame):
-	global running
-	syslog("handler hit")
-	running = False
+    global running
+    syslog("handler hit")
+    running = False
+
 
 times = 0
 
--- flask-alarm.py
+++ flask-alarm.py
@@ -9,14 +9,17 @@ import json
 from flask import render_template
 from flask import Flask
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path="")
+
 
 def lookup_alarms():
     cron = CronTab(user="pi")
     return [str(job) for job in cron if job.is_enabled()]
 
+
 def lookup_sounds():
-    return  [x.split(".")[0] for x in os.listdir('/home/pi/alarm/sounds')]
+    return [x.split(".")[0] for x in os.listdir("/home/pi/alarm/sounds")]
+
 
 @app.route("/")
 def hello():
@@ -27,8 +30,8 @@ def hello():
     :return: returns basic "hello world" string
     """
     sounds = lookup_sounds()
-    scheduled = [ x.split() for x in lookup_alarms()]
-    data = {"sounds":sounds, "scheduled":scheduled}
+    scheduled = [x.split() for x in lookup_alarms()]
+    data = {"sounds": sounds, "scheduled": scheduled}
     return render_template("index.html", **data)
 
 
@@ -38,7 +41,8 @@ def running_alarms():
     gets list of running alarms
     :return: returns list of running alarms
     """
-    return json.dumps(os.listdir('/home/pi/alarm/alarms'))
+    return json.dumps(os.listdir("/home/pi/alarm/alarms"))
+
 
 @app.route("/set_alarm/<newalarm>")
 def set_alarm(newalarm):
@@ -47,12 +51,12 @@ def set_alarm(newalarm):
     :param newalarm: a string representing the alarm to dset
     :return: returns success or failure base on data
     """
-    print "got back", newalarm
+    print(f"got back: {newalarm}")
     days, hours, sound = newalarm.split("-")
     if not days or not hours or not sound:
         return json.dumps("Failure")
 
-    #determine days
+    # determine days
     day_string = []
     for x in days:
         if x == "U":
@@ -77,26 +81,29 @@ def set_alarm(newalarm):
     for day in day_string:
 
         cron = CronTab(user="pi")
-        job = cron.new(command='/home/pi/alarm/alarm.py {0}'.format(sound))
+        job = cron.new(command="/home/pi/alarm/alarm.py {0}".format(sound))
         job.dow.on(day)
 
-        #determine hours
+        # determine hours
         job.hour.on(times[0])
         job.minute.on(times[1])
-        #create new cron
+        # create new cron
         if job.is_valid():
             job.enable()
             cron.write()
 
     return json.dumps("success")
 
+
 @app.route("/get_alarms")
 def get_alarms():
     return json.dumps(lookup_alarms())
 
+
 @app.route("/sounds")
 def get_sounds():
     return json.dumps(lookup_sounds())
+
 
 @app.route("/kill/<int:pid>")
 def kill(pid):
@@ -108,4 +115,4 @@ def kill(pid):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
